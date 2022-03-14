b95507a200601880642094ee6bbae4eb4765ec89
229939 Lewandowski Jakub <229939@edu.p.lodz.pl>
2022-03-14 22:13:54 +0100

    Pierwsza wersja projektu

---
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.it.ssbd2022.ssbd03;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
