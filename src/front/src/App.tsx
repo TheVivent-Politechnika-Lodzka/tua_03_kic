1464014fede71cbe5fe707f71471c9581baa7b3a
229963 Daniel Modrzejewski <229963@edu.p.lodz.pl>
2022-04-25 12:12:21 +0200

    Revert "Wstepna wersja strony gównej i struktury plików"
    This reverts commit f2b6c75b76b7d98ca418a0aecff3b88639db568a.

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/commonPages/MainPage/MainPage";
-import ErrorPage from "./pages/commonPages/ErrorPage/ErrorPage";
-import LoginPage from "./pages/commonPages/LoginPage/LoginPage";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <>
       <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/*" element={<ErrorPage />} />
-        </Routes>
+          <Routes>
+                 <Route path="/" element={<MainPage />} />
+                 <Route path="/*" element={<ErrorPage />} />
+          </Routes>
       </Router>
-    </>
-  );
+);
 }
 
 export default App;
