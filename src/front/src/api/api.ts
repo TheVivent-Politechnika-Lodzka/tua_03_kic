4a67fa41b49247c119904f254b8aee2b152e7180
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-05-04 20:36:44 +0200

    zmiana wywołania API na użycie RTK Query

---
+++ src/front/src/api/api.ts
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import jwtDecode from "jwt-decode";
+
+// TODO przenieść do .env / package.json
+// const BASE_URL = "http://studapp.it.p.lodz.pl:8003/api"
+const BASE_URL = "http://localhost:8080/api";
+const TOKEN_STORAGE_KEY = "AUTH_TOKEN";
+
+// TODO przenieść do własnego pliku
+export interface Credentials {
+  login: string;
+  password: string;
+}
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    // dodawaj token do wszystkich zapytan, jeżeli jest dostępny
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem(TOKEN_STORAGE_KEY);
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  // ENDPOINTY
+  endpoints: (builder) => ({
+    login: builder.mutation<string, Credentials>({
+      query: (credentials: Credentials) => ({
+        url: "/mok/login",
+        method: "POST",
+        body: credentials,
+        responseHandler: async (response) => {
+          if (response.ok) {
+            const token = await response.text();
+            localStorage.setItem(TOKEN_STORAGE_KEY, token);
+            return jwtDecode(token);
+          }
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = api;
