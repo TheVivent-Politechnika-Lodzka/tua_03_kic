2666a1c18c8f45159c782c5785251ed71e2785cd
230016 Szczeciński Damian <230016@edu.p.lodz.pl>
2022-05-15 01:18:22 +0200

    dodanie w AccountAlreadyExistsException buildera i stworzenie wyjątków dla loginu i emaila

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/exceptions/account/AccountAlreadyExistsException.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/exceptions/account/AccountAlreadyExistsException.java
@@ -6,8 +6,18 @@ import pl.lodz.p.it.ssbd2022.ssbd03.exceptions.AppBaseException;
 public class AccountAlreadyExistsException extends AppBaseException {
 
     private static final String ACCOUNT_ALREADY_EXISTS = "Account already exists";
+    private static final String LOGIN_ALREADY_EXISTS = "Account already exists";
+    private static final String EMAIL_ALREADY_EXISTS = "Account already exists";
 
-    public AccountAlreadyExistsException() {
-        super(ACCOUNT_ALREADY_EXISTS, Response.Status.BAD_REQUEST);
+    private AccountAlreadyExistsException(String message) {
+            super(message, Response.Status.CONFLICT);
+        }
+
+        static public AccountAlreadyExistsException loginExists() {
+            return new AccountAlreadyExistsException(LOGIN_ALREADY_EXISTS);
+        }
+
+        static public AccountAlreadyExistsException emailExists() {
+            return new AccountAlreadyExistsException(EMAIL_ALREADY_EXISTS);
+        }
     }
-}
