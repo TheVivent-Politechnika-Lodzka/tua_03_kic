3a8fcee8ca0f40cd763cff2f4b06af8add885ed4
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-05-17 10:19:29 +0200

    dodanie Serializera/Deserializera na Locale w CreateAccountDto

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/mok/dto/CreateAccountDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/mok/dto/CreateAccountDto.java
@@ -1,12 +1,14 @@
 package pl.lodz.p.it.ssbd2022.ssbd03.mok.dto;
 
 import jakarta.json.bind.annotation.JsonbTypeDeserializer;
+import jakarta.json.bind.annotation.JsonbTypeSerializer;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.lodz.p.it.ssbd2022.ssbd03.mappers.json.LocaleSerializerDeserializer;
 import pl.lodz.p.it.ssbd2022.ssbd03.validation.FirstName;
 import pl.lodz.p.it.ssbd2022.ssbd03.validation.LastName;
 import pl.lodz.p.it.ssbd2022.ssbd03.validation.Login;
@@ -41,6 +43,8 @@ public class CreateAccountDto extends AccountDto {
     @NotNull
     private boolean isConfirmed;
     @NotNull
+    @JsonbTypeDeserializer(LocaleSerializerDeserializer.class)
+    @JsonbTypeSerializer(LocaleSerializerDeserializer.class)
     private Locale language;
 
 }
