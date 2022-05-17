2fd1f8471c94a6c5a2a575da78773eda6edfca99
229977 Paulina Papiernik <229977@edu.p.lodz.pl>
2022-05-17 22:14:36 +0200

    Zaimplementowanie abstractManagera

---
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/common/AbstractManager.java
@@ -0,0 +1,68 @@
+package pl.lodz.p.it.ssbd2022.ssbd03.common;
+
+import jakarta.annotation.Resource;
+import jakarta.ejb.SessionContext;
+import jakarta.ejb.TransactionAttribute;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static jakarta.ejb.TransactionAttributeType.NOT_SUPPORTED;
+
+public class AbstractManager {
+
+    @Resource
+    SessionContext sessionContext;
+
+    protected static final Logger LOGGER = Logger.getGlobal();
+
+    private String transactionId;
+
+    private boolean lastTransactionRollback;
+
+    @TransactionAttribute(NOT_SUPPORTED)
+    public boolean isLastTransactionRollback() {
+        return lastTransactionRollback;
+    }
+
+    public void afterBegin() {
+        transactionId = Long.toString(System.currentTimeMillis())
+                + ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+        LOGGER.log(
+            Level.INFO,
+            "Transakcja o id={0}, rozpoczęta w: {1}; tożsamość: {2}",
+            new Object[] {
+                transactionId,
+                this.getClass().getName(),
+                sessionContext.getCallerPrincipal().getName()
+            }
+        );
+    }
+
+    public void beforeCompletion() {
+        LOGGER.log(
+            Level.INFO,
+            "Transakcja o id={0}, przed zatwierdzeniem w {1}; tożsamość: {2}",
+            new Object[] {
+                    transactionId,
+                    this.getClass().getName(),
+                    sessionContext.getCallerPrincipal().getName()
+            }
+        );
+    }
+
+    public void afterCompletion(boolean committed) {
+        lastTransactionRollback = !committed;
+        LOGGER.log(
+            Level.INFO,
+            "Transakcja o id={0}, zakończona w {1} (status: {2}); tożsamość {3}",
+            new Object[] {
+                transactionId,
+                this.getClass().getName(),
+                committed ? "ZATWIERDZONA" : "ODWOŁANA",
+                sessionContext.getCallerPrincipal().getName()
+            }
+        );
+    }
+}
