a0281e9ae905afa465bdd3c1e194cdc4565fb4c4
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-05-21 19:13:28 +0200

    ActiveAccountFacade: dodanie metody findExpiredTokens ConfirmationAccountToken: dodanie potrzebnego NamedQuery init.sql: dodanie uprawnień kontu mok do tabeli z tokenami Dodanie UnconfirmedAccountCleanerService mającego metodę czyszczącą niepotwierdzone konta co minutę

---
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/global_services/UnconfirmedAccountCleanerService.java
@@ -0,0 +1,37 @@
+package pl.lodz.p.it.ssbd2022.ssbd03.global_services;
+
+import jakarta.ejb.*;
+import jakarta.inject.Inject;
+import pl.lodz.p.it.ssbd2022.ssbd03.mok.ejb.facades.AccountFacade;
+import pl.lodz.p.it.ssbd2022.ssbd03.mok.ejb.facades.ActiveAccountFacade;
+
+/**
+ * Klasa odpowiedzialna za usuwanie niepotwierdzonych kont
+ */
+@Startup
+@Singleton
+public class UnconfirmedAccountCleanerService {
+
+    @Inject
+    private ActiveAccountFacade activeAccountFacade;
+
+    @Inject
+    private AccountFacade accountFacade;
+
+
+
+    /**
+     * Metoda wywoływana przez serwer aplikacji co minutę.
+     * Usuwa niepotwierdzone konta z nieważnymi tokenami.
+     */
+    @Schedule(hour = "*", minute = "*/1", persistent = false)
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    private void searchForTokens() {
+        System.out.println("UnconfirmedAccountCleanerService.searchForTokens()");
+        activeAccountFacade.findExpiredTokens().forEach((token) -> {
+            activeAccountFacade.unsafeRemove(token);
+            accountFacade.unsafeRemove(token.getAccount());
+        });
+    }
+
+}
