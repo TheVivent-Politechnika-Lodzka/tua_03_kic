e3f5f608e9508ca8f9ac60984187a4dea59d5fca
230016 Szczeciński Damian <230016@edu.p.lodz.pl>
2022-05-22 23:24:53 +0200

    dodanie strony importującej komponent zawierający formularz tworzenia konta przez administratora

---
+++ src/front/src/pages/admin/AdminPage/createAccountPage/CreateAccountPage.tsx
@@ -0,0 +1,9 @@
+import CreateAccountForm from "../../../../component/Form/createAccountForm/CreateAccountForm";
+
+export default function CreateAccountPage() {
+  return (
+    <div>
+      <CreateAccountForm />
+    </div>
+  );
+}
