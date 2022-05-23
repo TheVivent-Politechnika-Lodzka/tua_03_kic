66d7090db75f8ae86540cf5d8fcde181a98a6544
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-05-24 01:14:54 +0200

    minor changes poprawienie tabelki z użytkownikami dodanie dodawania i usuwania akces leweli poprawki w api.ts

--- src/front/src/api/types/apiParams.d.ts
+++ src/front/src/api/types/apiParams.d.ts
@@ -1,106 +1,107 @@
 import {
-  AbstractDto,
-  AccessLevelName,
-  Language,
-  PaginationData,
+    AbstractDto,
+    AccessLevelName,
+    Language,
+    PaginationData,
 } from "./common";
 
 interface ETag {
-  ETag: string;
+    ETag: string;
 }
 
 export interface RegisterClientDto {
-  password: string;
-  login: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  pesel: string;
-  language: Language;
+    password: string;
+    login: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    pesel: string;
+    language: Language;
 }
 
 export interface RegisterClientConfirmDto {
-  token: string;
-  //   login: string;
+    token: string;
+    //   login: string;
 }
 
 export interface CreateAccountDto {
-  password: string;
-  login: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  language: Language;
+    password: string;
+    login: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    language: Language;
 }
 
 export interface AccountActivationDto {
-  login: string;
-  tag: ETag;
+    login: string;
+    tag: ETag;
 }
 
 export interface AccessLevelDto {
-  level: AccessLevelName;
-  phoneNumber?: string;
-  contactEmail?: string;
-  pesel?: string;
+    level: AccessLevelName;
+    ETag?: string;
+    phoneNumber?: string;
+    contactEmail?: string;
+    pesel?: string;
 }
 
 export interface AddAccessLevel {
-  login: string;
-  accessLevel: AccessLevelDto;
+    login: string;
+    accessLevel: AccessLevelDto;
 }
 
 export interface RemoveAccessLevel {
-  login: string;
-  accessLevel: AccessLevelName;
-  eTag: string;
+    login: string;
+    accessLevel: AccessLevelName;
+    eTag: string;
 }
 
 export interface changeOwnPasswordDto {
-  oldPassword: string;
-  newPassword: string;
-  ETag: string;
+    oldPassword: string;
+    newPassword: string;
+    ETag: string;
 }
 
 export interface ChangePasswordDto {
-  login: string;
-  data: {
-    newPassword: string;
-    ETag: string;
-  };
+    login: string;
+    data: {
+        newPassword: string;
+        ETag: string;
+    };
 }
 
 export interface AccountWithAccessLevelsDto extends AbstractDto {
-  email: string;
-  login: string;
-  firstName: string;
-  lastName: string;
-  active: boolean;
-  confirmed: boolean;
-  language: Language;
-  accessLevels: AccessLevelDto[];
+    email: string;
+    login: string;
+    firstName: string;
+    lastName: string;
+    active: boolean;
+    confirmed: boolean;
+    language: Language;
+    accessLevels: AccessLevelDto[];
 }
 export interface AccountsPaginationData extends PaginationData {
-  data: AccountWithAccessLevelsDto[];
+    data: AccountWithAccessLevelsDto[];
 }
 
 export interface AccountWithAccessLevelsDtoWithLogin {
-  login: string;
-  data: AccountWithAccessLevelsDto;
+    login: string;
+    data: AccountWithAccessLevelsDto;
 }
 
 export interface LoginCredentials {
-  login: string;
-  password: string;
+    login: string;
+    password: string;
 }
 
 export interface JustLogin {
-  login: string;
+    login: string;
 }
 
 export interface ResetPasswordTokenDto {
-  login: string;
-  token: string;
-  password: string;
+    login: string;
+    token: string;
+    password: string;
 }
