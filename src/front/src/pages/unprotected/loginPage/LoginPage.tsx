d8f3726edb28d107b06bfd8dc1f43336c861eb27
230016 Szczeciński Damian <230016@edu.p.lodz.pl>
2022-05-24 00:58:12 +0200

    dodanie providera recaptchy dla rejestracji

--- src/front/src/pages/unprotected/loginPage/LoginPage.tsx
+++ src/front/src/pages/unprotected/loginPage/LoginPage.tsx
@@ -3,6 +3,7 @@ import LangSelect from "../../../component/Selector/LangBarSelect/LangSelect";
 import LoginForm from "../../../component/Form/loginForm/LoginForm";
 import RegistrationForm from "../../../component/Form/registerForm/RegistrationForm";
 import styles from "./loginPage.module.scss";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 const LoginPage = () => {
   return (
@@ -11,7 +12,9 @@ const LoginPage = () => {
       <LangSelect />
       <div className={styles.login_section}>
         <LoginForm />
-        <RegistrationForm />
+        <GoogleReCaptchaProvider reCaptchaKey="6Lf85hEgAAAAAONrGfo8SoQSb9GmfzHXTTgjKJzT">
+          <RegistrationForm />
+        </GoogleReCaptchaProvider>
       </div>
     </div>
   );
