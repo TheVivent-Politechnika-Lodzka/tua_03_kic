f478a2f0410372b8972cb6f8b0665f77df560c74
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-05-23 23:19:32 +0200

    bangla zmiana danych użytkownika

--- src/front/src/pages/account/details/EditAccountDetails.tsx
+++ src/front/src/pages/account/details/EditAccountDetails.tsx
@@ -1,11 +1,16 @@
 import style from "./accountDetails.module.scss";
 import { useState, useEffect } from "react";
-import { useGetOwnAccountDetailsWorkaroundMutation } from "../../../api/api";
+import {
+  useEditOwnAccountMutation,
+  useGetOwnAccountDetailsWorkaroundMutation,
+} from "../../../api/api";
 import { AccountWithAccessLevelsDto } from "../../../api/types/apiParams";
+import { Button } from "@mui/material";
 
 const EditAccountDetailsPage = () => {
-  const [getCurrentUser, { isLoading }] =
-    useGetOwnAccountDetailsWorkaroundMutation();
+  const [getCurrentUser] = useGetOwnAccountDetailsWorkaroundMutation();
+  const [saveCurrentUser] = useEditOwnAccountMutation();
+  const [message, setMessage] = useState("");
   const [currentUserData, setCurrentUserData] =
     useState<AccountWithAccessLevelsDto>({} as AccountWithAccessLevelsDto);
 
@@ -20,14 +25,45 @@ const EditAccountDetailsPage = () => {
     }
   };
 
+  const handleSubmit = async () => {
+    const res = await saveCurrentUser(currentUserData);
+    if ("data" in res) {
+      setCurrentUserData(res.data);
+    } else if ("error" in res && "data" in res.error) {
+      setMessage(res.error.data as string);
+    } else {
+      setMessage("error");
+    }
+  };
+
   return (
     <div className={style.whiteText}>
       <h1>Edit Account Details Page</h1>
       <div>
         <div>
-          imię: <input value={currentUserData.firstName} />
-          nazwisko: <input value={currentUserData.lastName} />
+          imię:{" "}
+          <input
+            value={currentUserData.firstName}
+            onChange={(e) =>
+              setCurrentUserData((old) => ({
+                ...old,
+                firstName: e.target.value,
+              }))
+            }
+          />
+          nazwisko:{" "}
+          <input
+            value={currentUserData.lastName}
+            onChange={(e) =>
+              setCurrentUserData((old) => ({
+                ...old,
+                lastName: e.target.value,
+              }))
+            }
+          />
         </div>
+        <Button onClick={handleSubmit}>zapisz</Button>
+        <div>{message}</div>
       </div>
     </div>
   );
