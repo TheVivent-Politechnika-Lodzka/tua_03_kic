642243ad0847d43bfaffc79a24b2fc0e48284df5
230016 Szczeciński Damian <230016@edu.p.lodz.pl>
2022-06-09 17:57:46 +0200

    Dodanie DTO dla Implant - ImplantDto.

---
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/mop/dto/ImplantDto.java
@@ -0,0 +1,39 @@
+package pl.lodz.p.it.ssbd2022.ssbd03.mop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import pl.lodz.p.it.ssbd2022.ssbd03.security.Taggable;
+import pl.lodz.p.it.ssbd2022.ssbd03.validation.*;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImplantDto implements Taggable {
+    private UUID id;
+    private Long version;
+
+    @Name
+    private String name;
+
+    @Description
+    private String description;
+
+    @Manufacturer
+    private String manufacturer;
+
+    @Price
+    private int price;
+
+    private boolean archived;
+
+    private int popularity;
+
+    private Duration duration;
+
+    @Url
+    private String image;
+}
