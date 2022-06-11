4420d0c7685873955027a076b43cb345aae2e8fe
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-06-11 13:49:09 +0200

    Zaimplementowano dodawanie recenzji do bazy danych

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/entities/ImplantReview.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/entities/ImplantReview.java
@@ -1,4 +1,5 @@
 package pl.lodz.p.it.ssbd2022.ssbd03.entities;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -7,7 +8,6 @@ import lombok.Setter;
 import lombok.ToString;
 import pl.lodz.p.it.ssbd2022.ssbd03.common.AbstractEntity;
 import pl.lodz.p.it.ssbd2022.ssbd03.entities.access_levels.DataClient;
-import pl.lodz.p.it.ssbd2022.ssbd03.validation.Description;
 import pl.lodz.p.it.ssbd2022.ssbd03.validation.Rating;
 import pl.lodz.p.it.ssbd2022.ssbd03.validation.Review;
 
@@ -15,16 +15,12 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(
-        name="implant_review",
-        indexes = {
-                @Index(name = "review_client_id", columnList = "client_id"),
-                @Index(name = "review_implant_id", columnList = "implant_id")
-        },
-        uniqueConstraints = {
-                @UniqueConstraint(name = "review_client_id_implant_id", columnNames = {"client_id", "implant_id"})
-        }
-)
+@Table(name = "implant_review", indexes = {
+        @Index(name = "review_client_id", columnList = "client_id"),
+        @Index(name = "review_implant_id", columnList = "implant_id")
+}, uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"client_id", "implant_id"}, name = ImplantReview.CONSTRAINT_ONE_REVIEW_PER_CLIENT_PER_IMPLANT)
+})
 @NamedQueries({
         @NamedQuery(name = "Review.findAll", query = "select a from ImplantReview a"),
         @NamedQuery(name = "Review.findById", query = "select a from ImplantReview a where a.id = :id"),
@@ -35,6 +31,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class ImplantReview extends AbstractEntity implements Serializable {
 
+    public static final String CONSTRAINT_ONE_REVIEW_PER_CLIENT_PER_IMPLANT = "one_review_per_client_per_implant";
     private static final long serialVersionUID = 1L;
 
     @JoinColumn(name = "implant_id", referencedColumnName = "id", updatable = false)
@@ -52,7 +49,7 @@ public class ImplantReview extends AbstractEntity implements Serializable {
     private DataClient client;
 
     @Basic(optional = false)
-    @Column(name = "text", nullable = false, length = 1024)
+    @Column(name = "review", nullable = false, length = 1000)
     @Getter
     @Setter
     @Review
