46d3b0a5ed45ca2f1cc275dcc95ca4822aab072e
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-06-14 01:03:38 +0200

    Od teraz navbar przekierowuje do strony logowania oraz rejestracji

--- src/front/src/components/Navbar/Navbar.tsx
+++ src/front/src/components/Navbar/Navbar.tsx
@@ -2,25 +2,47 @@ import avatar from "../../assets/images/avatar.jpg";
 import { useStoreSelector } from "../../redux/reduxHooks";
 import UserAccessLevel from "./UserAccessLevel";
 import "./style.scss";
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
     const login = useStoreSelector((state) => state.user.sub);
     const accessLevel = useStoreSelector((state) => state.user.cur);
+    const navigate = useNavigate();
 
     return (
         <nav className={`navbar ${accessLevel.toLowerCase()}`}>
-            {/* <Select className="language-select" data={languageSelect} /> */}
-            <div className="user-info-wrapper">
-                <div className="user-text-wrapper">
-                    <p className="user-login">{login}</p>
-                    <UserAccessLevel accessLevel={accessLevel} />
+            {accessLevel ? (
+                <div className="user-info-wrapper">
+                    <div className="user-text-wrapper">
+                        <p className="user-login">{login}</p>
+                        <UserAccessLevel accessLevel={accessLevel} />
+                    </div>
+                    <img
+                        src={avatar}
+                        alt="Avatar użytkownika"
+                        className="user-avatar"
+                    />
                 </div>
-                <img
-                    src={avatar}
-                    alt="Avatar użytkownika"
-                    className="user-avatar"
-                />
-            </div>
+            ) : (
+                <div className="navbar-button-wrapper">
+                    <div
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                        className="navbar-button-login"
+                    >
+                        Zaloguj
+                    </div>
+                    <div
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                        className="navbar-button-register"
+                    >
+                        Zarejestruj się
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
