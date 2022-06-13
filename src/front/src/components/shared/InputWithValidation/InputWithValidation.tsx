986a31c7eb12437abc79d229c2489158d7c3b3a0
229907 Adam Kapuściński <229907@edu.p.lodz.pl>
2022-06-14 01:24:08 +0200

    zmiana tabulacji (🙃) dodanie callbacka do InputWithValidation. Teraz można zrobić coś z wartością weryfikowaną

--- src/front/src/components/shared/InputWithValidation/InputWithValidation.tsx
+++ src/front/src/components/shared/InputWithValidation/InputWithValidation.tsx
@@ -2,48 +2,62 @@ import { useContext, useState } from "react";
 import { faCheck, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style.scss";
-import { ActionType, validationContext } from "../../../context/validationContext";
+import {
+    ActionType,
+    validationContext,
+} from "../../../context/validationContext";
 
 interface InputWithValidationProps {
-  title: string;
-  value: string | undefined;
-  validationType: ActionType;
-  isValid: boolean;
+    title: string;
+    value: string | undefined;
+    validationType: ActionType;
+    isValid: boolean;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputWithValidation = ({
-  title,
-  value,
-  validationType,
-  isValid,
+    title,
+    value,
+    validationType,
+    isValid,
+    onChange,
 }: InputWithValidationProps) => {
-  const [input, setInput] = useState<string | undefined>(value);
-  const { state, dispatch } = useContext(validationContext);
+    const [input, setInput] = useState<string | undefined>(value);
+    const { state, dispatch } = useContext(validationContext);
 
-  return (
-    <div className="edit-field-wrapper">
-      <p className="edit-field-title">{title}</p>
-      <div className="input-wrapper">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-            dispatch({
-              type: validationType,
-              payload: { ...state, input: e.target.value },
-            });
-          }}
-          className={`edit-field-input ${isValid ? "valid" : "invalid"}`}
-        />
-        <FontAwesomeIcon
-          className={`edit-field-icon ${isValid ? "valid" : "invalid"}`}
-          icon={isValid ? faCheck : faClose}
-          color="#00FF66"
-        />
-      </div>
-    </div>
-  );
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+        dispatch({
+            type: validationType,
+            payload: { ...state, input: e.target.value },
+        });
+        if (onChange) {
+            onChange(e);
+        }
+    };
+
+    return (
+        <div className="edit-field-wrapper">
+            <p className="edit-field-title">{title}</p>
+            <div className="input-wrapper">
+                <input
+                    type="text"
+                    value={input}
+                    onChange={handleChange}
+                    className={`edit-field-input ${
+                        isValid ? "valid" : "invalid"
+                    }`}
+                />
+                <FontAwesomeIcon
+                    className={`edit-field-icon ${
+                        isValid ? "valid" : "invalid"
+                    }`}
+                    icon={isValid ? faCheck : faClose}
+                    color="#00FF66"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default InputWithValidation;
