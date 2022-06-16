67b0b1c6d6fafd61b7687688c00278335b5a1028
229907 Kapuściński Adam <229907@edu.p.lodz.pl>
2022-06-16 16:42:53 +0200

    dodanie walidacji do CreateAppointmentDto

--- src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/mop/dto/CreateAppointmentDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2022/ssbd03/mop/dto/CreateAppointmentDto.java
@@ -1,5 +1,6 @@
 package pl.lodz.p.it.ssbd2022.ssbd03.mop.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,13 @@ import java.util.UUID;
 @AllArgsConstructor
 public class CreateAppointmentDto {
 
+    @NotNull(message = "server.error.validation.constraints.notNull.createAppointment.specialist")
     private UUID specialistId;
+
+    @NotNull(message = "server.error.validation.constraints.notNull.createAppointment.implant")
     private UUID implantId;
+
+    @NotNull(message = "server.error.validation.constraints.notNull.createAppointment.startDate")
     private Instant startDate;
 
 }
