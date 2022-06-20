101339d3633957b395a70826ef1e52e92d331b90
JLewandowski0 <229939@edu.p.lodz.pl>
2022-06-20 23:03:36 +0200

    Mop 10 Front DONE

--- src/front/src/pages/protected/shared/EditOwnAppointment/EditOwnAppointment.tsx
+++ src/front/src/pages/protected/shared/EditOwnAppointment/EditOwnAppointment.tsx
@@ -1,11 +1,18 @@
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { ChronoUnit, Instant, LocalDateTime } from "@js-joda/core";
+import {
+    ChronoUnit,
+    Instant,
+    LocalDateTime,
+    ZoneId,
+    ZoneOffset,
+} from "@js-joda/core";
 import { showNotification } from "@mantine/notifications";
 import { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
 import { useNavigate, useParams } from "react-router";
 import {
     AppointmentListElementDto,
+    editOwnAppointment,
     getAppointmentDetails,
 } from "../../../../api/mop";
 import ActionButton from "../../../../components/shared/ActionButton/ActionButton";
@@ -16,10 +23,13 @@ import styles from "./style.module.scss";
 export const EditOwnAppointment = () => {
     const aLevel = useStoreSelector((state) => state.user.cur);
     const [appointment, setAppointment] = useState<AppointmentListElementDto>();
+    const [etag, setEtag] = useState<string>("");
+    const [inputStatus, setInputStatus] = useState<string>('')
     const [loading, setLoading] = useState<Loading>({
         pageLoading: true,
         actionLoading: false,
     });
+    const [inputDescription, setInputDescription] = useState<string>("");
     const [startDate, setStartDate] = useState<string>("");
     const [endDate, setEndDate] = useState<string>("");
     let { id } = useParams();
@@ -32,30 +42,50 @@ export const EditOwnAppointment = () => {
             showNotification(failureNotificationItems(data.errorMessage));
             return;
         }
+        setEtag(data.etag);
+        setInputStatus(data.data.status)
+        setInputDescription(data.data.description);
         setAppointment(data.data);
         setLoading({ pageLoading: false, actionLoading: false });
     };
     useEffect(() => {
         handleGetAppointmentDetails();
     }, []);
+    const handleEditOwnAppointment = async () => {
+        if (!id || !etag || !appointment) return;
+        let status;
+        if (aLevel === "SPECIALIST") status = inputStatus;
+        else status = appointment.status;
+        const version = appointment.version;
+        let description;
+        if (aLevel === "SPECIALIST") description = inputDescription;
+        else description = appointment.description;
+        let editedDate = LocalDateTime.parse(startDate).toInstant(
+            ZoneOffset.of(ZoneId.UTC.id())
+        );
+        editedDate = editedDate.minus(2, ChronoUnit.HOURS)
+        const data = await editOwnAppointment({
+            id,
+            etag,
+            status,
+            version,
+            description,
+            startDate: editedDate,
+        });
+        if ("errorMessage" in data) {
+            showNotification(failureNotificationItems(data.errorMessage));
+            return;
+        }
+        navigate('/visits')
+    };
     useEffect(() => {
         if (!appointment) return;
         const startDate = Instant.parse(appointment.startDate);
         const fixedStartDate = LocalDateTime.ofInstant(startDate);
         const endDate = Instant.parse(appointment.endDate);
         const fixedEndDate = LocalDateTime.ofInstant(endDate);
-        setStartDate(
-            fixedStartDate
-                .truncatedTo(ChronoUnit.MINUTES)
-                .toString()
-                .replace("T", " ")
-        );
-        setEndDate(
-            fixedEndDate
-                .truncatedTo(ChronoUnit.MINUTES)
-                .toString()
-                .replace("T", " ")
-        );
+        setStartDate(fixedStartDate.truncatedTo(ChronoUnit.MINUTES).toString());
+        setEndDate(fixedEndDate.truncatedTo(ChronoUnit.MINUTES).toString());
     }, [appointment]);
     return (
         <div className={styles.edit_appointment_page}>
@@ -70,7 +100,7 @@ export const EditOwnAppointment = () => {
             ) : (
                 <div className={styles.account_details}>
                     <p className={styles.account_details_title}>
-                        Szczegóły wizyty
+                        Edytuj wizytę
                     </p>
                     <div className={styles.details_wrapper}>
                         <div className={styles.detail_wrapper}>
@@ -100,6 +130,15 @@ export const EditOwnAppointment = () => {
                             <p className={styles.description}>
                                 {appointment?.description}
                             </p>
+                            {aLevel === "SPECIALIST" && (
+                                <input className={styles.description_input}
+                                    type="text"
+                                    value={inputDescription}
+                                    onChange={(e) =>
+                                        setInputDescription(e.target.value)
+                                    }
+                                />
+                            )}
                         </div>
                         <div className={styles.detail_wrapper}>
                             <p className={styles.title}>Cena wizyty:</p>
@@ -112,6 +151,12 @@ export const EditOwnAppointment = () => {
                                 Data rozpoczęcia wizyty:
                             </p>
                             <p className={styles.description}>{startDate}</p>
+                            <input
+                                type="datetime-local"
+                                onChange={(e) => {
+                                    setStartDate(e.target.value);
+                                }}
+                            />
                         </div>
                         <div className={styles.detail_wrapper}>
                             <p className={styles.title}>
@@ -124,14 +169,31 @@ export const EditOwnAppointment = () => {
                             <p className={styles.description}>
                                 {appointment?.status}
                             </p>
+                            {aLevel === "SPECIALIST" && (
+                                <>
+                                <input
+                                    type="checkbox"
+                                    value={inputDescription}
+                                    onChange={(e) =>
+                                        {
+                                        if(e.target.value) setInputStatus('ACCEPTED')
+                                        else {setInputStatus('PENDIG')}
+                                    }
+                                    }
+                                />
+                                <p className={styles.title}>Akceptuj wizytę</p>
+                                </>
+                            )}
                         </div>
                         <div className={styles.detail_wrapper}>
-                            <ActionButton
-                                title="Edytuj wizyte"
-                                color="purple"
-                                icon={faInfoCircle}
-                                onClick={() => console.log('essa')}
-                            ></ActionButton>
+                            {
+                                <ActionButton
+                                    title="Prześlij zmianę"
+                                    color="purple"
+                                    icon={faInfoCircle}
+                                    onClick={() => handleEditOwnAppointment()}
+                                ></ActionButton>
+                            }
                         </div>
                     </div>
                 </div>
