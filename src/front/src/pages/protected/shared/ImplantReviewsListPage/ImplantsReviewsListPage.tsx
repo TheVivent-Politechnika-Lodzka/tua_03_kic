7c04b5591085758d1e0e6d024e9a07aa554dc1e3
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-06-21 07:19:54 +0200

    Skończony MOP.18 od strony frontu

--- src/front/src/pages/protected/shared/ImplantReviewsListPage/ImplantsReviewsListPage.tsx
+++ src/front/src/pages/protected/shared/ImplantReviewsListPage/ImplantsReviewsListPage.tsx
@@ -12,6 +12,9 @@ import {
 } from "../../../../api";
 import { showNotification } from "@mantine/notifications";
 import { failureNotificationItems } from "../../../../utils/showNotificationsItems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import Pagination from "../../../../components/Pagination/Pagination";
 
 interface ImplantReviewsListPageProps {
     isOpened: boolean;
@@ -29,11 +32,20 @@ const ImplantReviewsListPage = ({
         pageLoading: true,
         actionLoading: false,
     });
+    const [pagination, setPagination] = useState<Pagination>({
+        currentPage: 1,
+        pageSize: 1,
+        totalPages: 1,
+    });
 
     const handleGetImplantReviews = async () => {
         if (!implantId) return;
         setLoading({ ...loading, pageLoading: true });
-        const data = await getImplantsReviews(implantId, 1, 5);
+        const data = await getImplantsReviews(
+            implantId,
+            pagination.currentPage as number,
+            pagination.pageSize as number
+        );
         if ("errorMessage" in data) {
             setLoading({ ...loading, pageLoading: false });
             showNotification(failureNotificationItems(data.errorMessage));
@@ -45,7 +57,7 @@ const ImplantReviewsListPage = ({
 
     useEffect(() => {
         handleGetImplantReviews();
-    }, [isOpened]);
+    }, [isOpened, pagination]);
 
     return (
         <Modal isOpen={isOpened}>
@@ -58,10 +70,26 @@ const ImplantReviewsListPage = ({
                 />
             ) : (
                 <div className={styles.implants_reviews_list_page}>
+                    <FontAwesomeIcon
+                        className={styles.close_icon}
+                        icon={faClose}
+                        onClick={onClose}
+                    />
                     <div className={styles.reviews_wrapper}>
                         {reviews?.data.map((review) => (
                             <ImplantReview key={review?.id} review={review} />
                         ))}
+                        {reviews?.data.length !== 0 && (
+                            <Pagination
+                                pagination={pagination}
+                                handleFunction={setPagination}
+                            />
+                        )}
+                        {reviews?.data.length === 0 && (
+                            <p className={styles.no_reviews}>
+                                Implant nie posiada żadnych recenzji
+                            </p>
+                        )}
                     </div>
                 </div>
             )}
